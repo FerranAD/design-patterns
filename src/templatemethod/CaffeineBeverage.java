cb93bf26474e4de1384d8c921820ee8ee8671b9f
FerranAD <farandomingo@gmail.com>
2022-07-06 13:32:19 +0200

    feat(template-method): add basic template method implementation

---
+++ src/templatemethod/CaffeineBeverage.java
@@ -0,0 +1,23 @@
+package templatemethod;
+
+public abstract class CaffeineBeverage {
+
+    public void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    public void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    public void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+    public abstract void brew();
+
+    public abstract void addCondiments();
+}
